51994c8ebe458926eb92ebf6654e28c7ef551648
BalamiRR <fuatkara.95@gmail.com>
2025-05-26 20:46:24 +0200

    A controller has been created for each class

--- src/main/java/com/openclassrooms/safetynet/SafetynetApplication.java
+++ src/main/java/com/openclassrooms/safetynet/SafetynetApplication.java
@@ -8,7 +8,7 @@ public class SafetynetApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SafetynetApplication.class, args);
-		System.out.println("Hello World!");
+		System.out.println("Hello Worlddddddddddddddd !");
 	}
 
 }
--- src/main/java/com/openclassrooms/safetynet/model/MedicalRecord.java
+++ src/main/java/com/openclassrooms/safetynet/model/MedicalRecord.java
@@ -1,7 +1,6 @@
 package com.openclassrooms.safetynet.model;
 
 import org.springframework.stereotype.Component;
-
 import java.util.Date;
 import java.util.List;
 
