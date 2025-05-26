9091914d8e5eda74e29764da0a33e56f7c7023d2
BalamiRR <fuatkara.95@gmail.com>
2025-05-26 18:26:52 +0200

    Station Model DONE

--- src/main/java/com/openclassrooms/safetynet/model/FireStation.java
+++ src/main/java/com/openclassrooms/safetynet/model/FireStation.java
@@ -1,4 +1,43 @@
 package com.openclassrooms.safetynet.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class FireStation {
+    //"address": "1509 Culver St",
+    //"station": "3"
+    private String address;
+    private String station;
+
+    public FireStation() {
+    }
+
+    public FireStation(String address, String station) {
+        this.address = address;
+        this.station = station;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStation() {
+        return station;
+    }
+
+    public void setStation(String station) {
+        this.station = station;
+    }
+
+    @Override
+    public String toString() {
+        return "FireStation{" +
+                "address='" + address + '\'' +
+                ", station='" + station + '\'' +
+                '}';
+    }
 }
