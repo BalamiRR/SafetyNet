567dbf8c27f9fe7303cc44e80937c45379d91f6d
BalamiRR <fuatkara.95@gmail.com>
2025-05-26 20:30:21 +0200

    MedicalRecord Model DONE

--- src/main/java/com/openclassrooms/safetynet/model/MedicalRecord.java
+++ src/main/java/com/openclassrooms/safetynet/model/MedicalRecord.java
@@ -1,4 +1,79 @@
 package com.openclassrooms.safetynet.model;
 
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
 public class MedicalRecord {
+
+    private String firstName;
+    private String lastName;
+    private Date birthdate;
+    private List<String> medications;
+    private List<String> allergies;
+
+    public MedicalRecord(){
+
+    }
+
+    public MedicalRecord(String firstName, String lastName, Date birthdate, List<String> medications, List<String> allergies) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+        this.medications = medications;
+        this.allergies = allergies;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public List<String> getMedications() {
+        return medications;
+    }
+
+    public List<String> getAllergies() {
+        return allergies;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public void setMedications(List<String> medications) {
+        this.medications = medications;
+    }
+
+    public void setAllergies(List<String> allergies) {
+        this.allergies = allergies;
+    }
+
+    @Override
+    public String toString() {
+        return "MedicalRecord{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthdate=" + birthdate +
+                ", medications=" + medications +
+                ", allergies=" + allergies +
+                '}';
+    }
 }
