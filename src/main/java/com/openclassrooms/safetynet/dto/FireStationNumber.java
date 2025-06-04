0c1c4765e1d96887c80512899ee2c2fda9b33849
BalamiRR <fuatkara.95@gmail.com>
2025-06-04 11:56:50 +0200

    Creating the dto class for childAlert

---
+++ src/main/java/com/openclassrooms/safetynet/dto/FireStationNumber.java
@@ -0,0 +1,45 @@
+package com.openclassrooms.safetynet.dto;
+
+import com.openclassrooms.safetynet.model.Person;
+
+import java.util.List;
+
+public class FireStationNumber {
+    private List<Person> listPerson;
+    private int adult;
+    private int child;
+
+    public FireStationNumber(){
+
+    }
+
+    public FireStationNumber(List<Person> listPerson, int adult, int child) {
+        this.listPerson = listPerson;
+        this.adult = adult;
+        this.child = child;
+    }
+
+    public List<Person> getListPerson() {
+        return listPerson;
+    }
+
+    public void setListPerson(List<Person> listPerson) {
+        this.listPerson = listPerson;
+    }
+
+    public int getAdult() {
+        return adult;
+    }
+
+    public void setAdult(int adult) {
+        this.adult = adult;
+    }
+
+    public int getChild() {
+        return child;
+    }
+
+    public void setChild(int child) {
+        this.child = child;
+    }
+}
