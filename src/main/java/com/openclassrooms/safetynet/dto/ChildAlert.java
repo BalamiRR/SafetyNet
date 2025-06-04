6415cd9a9c32401e788071de93e4ce50c4ae679c
BalamiRR <fuatkara.95@gmail.com>
2025-06-04 20:52:58 +0200

    Children Dto creation

--- src/main/java/com/openclassrooms/safetynet/dto/ChildAlert.java
+++ src/main/java/com/openclassrooms/safetynet/dto/ChildAlert.java
@@ -1,4 +1,35 @@
 package com.openclassrooms.safetynet.dto;
 
+import com.openclassrooms.safetynet.model.Person;
+
+import java.util.List;
+
 public class ChildAlert {
+    private List<Person> familyMembers;
+    private List<ChildrenDto> children;
+
+    public ChildAlert(){
+
+    }
+
+    public ChildAlert(List<Person> familyMembers, List<ChildrenDto> children) {
+        this.familyMembers = familyMembers;
+        this.children = children;
+    }
+
+    public List<Person> getFamilyMembers() {
+        return familyMembers;
+    }
+
+    public void setFamilyMembers(List<Person> familyMembers) {
+        this.familyMembers = familyMembers;
+    }
+
+    public List<ChildrenDto> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<ChildrenDto> children) {
+        this.children = children;
+    }
 }
