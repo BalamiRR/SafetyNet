0ecec796a2f2bace1d00d3286b262e97d4368159
BalamiRR <fuatkara.95@gmail.com>
2025-06-16 22:21:42 +0200

    Lombok configuration

--- src/main/java/com/openclassrooms/safetynet/model/JsonData.java
+++ src/main/java/com/openclassrooms/safetynet/model/JsonData.java
@@ -1,31 +1,22 @@
 package com.openclassrooms.safetynet.model;
 
+import lombok.Getter;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 public class JsonData {
+
     public static List<Person> person = new ArrayList<>();
     public static List<FireStation> firestation = new ArrayList<>();
     public static List<MedicalRecord> medicalrecord = new ArrayList<>();
-
-    public static List<Person> getPerson() {
-        return person;
-    }
 
     public static void setPerson(List<Person> person) {
         JsonData.person = person;
     }
 
-    public static List<FireStation> getFirestation() {
-        return firestation;
-    }
-
     public static void setFirestation(List<FireStation> firestation) {
         JsonData.firestation = firestation;
-    }
-
-    public static List<MedicalRecord> getMedicalrecord() {
-        return medicalrecord;
     }
 
     public static void setMedicalrecord(List<MedicalRecord> medicalrecord) {
