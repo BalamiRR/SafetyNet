f4b13e9dfb9762653e338f435d6d7d7d0e532ede
BalamiRR <fuatkara.95@gmail.com>
2025-07-01 16:38:51 +0200

    Javadoc for MedicalRecord

--- src/main/java/com/openclassrooms/safetynet/repository/MedicalRecordRepository.java
+++ src/main/java/com/openclassrooms/safetynet/repository/MedicalRecordRepository.java
@@ -16,6 +16,11 @@ public class MedicalRecordRepository {
         return jsonDataConverter.getMedicalRecord();
     }
 
+    /**
+     * Saves a new medical record if a record with the same first and last name doesn't already exist.
+     * @param medicalRecord The medical record to be saved.
+     * @return true if the record was added; false if it already exists or input is invalid.
+     */
     public Boolean saveMedicalRecord(MedicalRecord medicalRecord){
         boolean foundMedicalRecord = false;
         if(medicalRecord.getFirstName() == null || medicalRecord.getLastName() == null){
@@ -33,6 +38,14 @@ public class MedicalRecordRepository {
         return !foundMedicalRecord;
     }
 
+    /**
+     * Updates an existing medical record identified by first name and last name.
+     *
+     * @param firstName The first name of the person whose record will be updated.
+     * @param lastName The last name of the person whose record will be updated.
+     * @param medicalRecord The medical record containing updated information.
+     * @return true if the record was found and updated; false otherwise.
+     */
     public Boolean updateMedicalRecord(String firstName, String lastName, MedicalRecord medicalRecord){
         for(MedicalRecord medicalRecordA : this.getAllMedicalRecord()){
             if(medicalRecordA.getFirstName().equals(firstName) && medicalRecordA.getLastName().equals(lastName)){
@@ -44,7 +57,13 @@ public class MedicalRecordRepository {
         }
         return false;
     }
-
+    /**
+     * Deletes a medical record by matching first name and last name.
+     *
+     * @param firstName The first name of the medical record to delete.
+     * @param lastName The last name of the medical record to delete.
+     * @return true if a record was found and deleted; false otherwise.
+     */
     public Boolean deleteMedicalRecord(String firstName, String lastName){
         if(firstName == null && lastName == null){
             return false;
@@ -56,7 +75,6 @@ public class MedicalRecordRepository {
         }
         return false;
     }
-
 
     public MedicalRecord findMedicalRecordByName(String name, String lastName){
         for(MedicalRecord medicalRecord : this.getAllMedicalRecord()){
